6e85ef584d7bbed56185077ee243c11a3136c3ab
Carlos0101py <73728755+Carlos0101py@users.noreply.github.com>
2022-12-17 02:05:15 -0300

    mudança

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ import {
     Color,
     PerspectiveCamera,
     PointLight,
+    DirectionalLight,
     WebGLRenderer,
 } from 'https://unpkg.com/three/build/three.module.js';
 import {OrbitControls} from'https://unpkg.com/three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'https://unpkg.com/three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'https://unpkg.com/three/examples/jsm/loaders/MTLLoader';
  
+//Criação da cena
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth,  window.innerHeight );
@@ -18,7 +20,7 @@ const scene = new Scene();
 scene.background = new Color(0xdddddd);
 
 const camera = new PerspectiveCamera(
-    45,
+    60,
     window.innerWidth / window.innerHeight,
     1,
     2000
@@ -36,21 +38,31 @@ controls.enableZoom = true;
 controls.maxPolarAngle = 1.5;
 controls.enablePan = false
 
-const linght1 = new PointLight(0xc4c4c4, 1);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 2.0;
+
+//DirectionalLight
+
+const linght1 = new DirectionalLight(0xc4c4c4, 0.6);
 linght1.position.set(0,300,500)
 scene.add(linght1);
 
-const linght2 = new PointLight(0xc4c4c4, 1);
+const linght2 = new DirectionalLight(0xc4c4c4, 0.6);
 linght2.position.set(500, 100, 0)
 scene.add(linght2);
 
-const linght3 = new PointLight(0xc4c4c4, 1);
+const linght3 = new DirectionalLight(0xc4c4c4, 0.6);
 linght3.position.set(0,100,-500)
 scene.add(linght3);
 
-const linght4 = new PointLight(0xc4c4c4, 1);
+const linght4 = new DirectionalLight(0xc4c4c4, 0.6);
 linght4.position.set(-500,300,100)
 scene.add(linght4);
+
+
+
+
+//Carregamento de objetos
 
 const mtlLoader = new MTLLoader();
 mtlLoader.setPath('../img/');
@@ -63,7 +75,7 @@ mtlLoader.load('',(materials) =>{
     objLoader.load(
     'teamugobj.obj',
  (object) =>{
-    object.scale.set(200,200,200);
+    object.scale.set(250,250,250);
     scene.add(object)
 },
 (xhr) =>{
